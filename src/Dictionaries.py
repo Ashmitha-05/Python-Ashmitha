85748d1821cdfe56bc6e97c2879a5dc56cd9f6ba
Ashmitha-05 <ashmitha.v@diggibyte.com>
2026-07-10 20:17:44 +0530

    Modified dictionary operations and frequency count

--- src/Dictionaries.py
+++ src/Dictionaries.py
@@ -8,3 +8,31 @@ print(student)
 print(student["name"])
 print(student.get("age"))
 
+student = {
+    "name": "Ashmitha",
+    "age": 22,
+    "city": "Hosur"
+}
+
+del student["city"]
+
+print(student)
+
+numbers = [1, 2, 2, 3, 1, 4, 2]
+
+freq = {}
+
+for num in numbers:
+    freq[num] = freq.get(num, 0) + 1
+
+print(freq)
+
+marks = {
+    "Math": 85,
+    "Science": 92,
+    "English": 78
+}
+
+maximum = max(marks.values())
+
+print(maximum)
